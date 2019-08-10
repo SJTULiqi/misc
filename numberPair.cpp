a9a8a118b41595045072d141856de0962d429195
liqi <lickeymouses@163.com>
2019-08-10 15:52:56 +0800

    牛客网 数对

--- numberPair.cpp
+++ numberPair.cpp
@@ -1,22 +1,27 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
+
+typedef long long LLong;
 
 int main()
 {
     int n, k;
     cin >> n >> k;
-    int count = 0;
-    int y_start = max(k,1);
-    for(int x = 1; x <= n; ++x)
+    // 使用long long的原因是可能会超出int的表示范围，而long字节数不确定，long long确定是64位的
+	LLong count = 0;
+    if(k == 0)
     {
-        for(int y = 1; y <= n; ++y)
-        {
-			if ((x % y) >= k)
-			{
-				//cout << x << ' ' << y << endl;
-				count++;
-			}
-        }
+        // 牛客网不支持在这里写成 long long(n) ，所以 typedef 一下
+		count = LLong(n) * LLong(n);
+        cout << count;
+        return 0;
+    }
+
+    for(int y = k + 1; y <= n; ++y)
+    {
+        int division = n / y;
+        count += (y - k) * division + max(0, n - division * y - k + 1);
     }
     cout << count << endl;
     return 0;
